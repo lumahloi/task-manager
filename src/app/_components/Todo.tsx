5d02bdc95921c967a741a621f724c2055beeb55f
lumahloi <magusoflowers0803@gmail.com>
2025-05-28 11:58:27 -0300

    refactor edit

---
+++ src/app/_components/Todo.tsx
@@ -0,0 +1,160 @@
+"use client";
+import { useState } from "react";
+
+import { trpc } from "../_trpc/client";
+import { serverClient } from "../_trpc/serverClient";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Trash2, Pencil, Check } from "lucide-react";
+
+export default function Todo({
+  todo,
+}: {
+  todo: Awaited<ReturnType<(typeof serverClient)["getTodos"]>>[number];
+}) {
+  const getTodos = trpc.getTodos.useQuery(undefined, {
+    // initialData: todo,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
+
+  const setDone = trpc.setDone.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
+
+  const deleteTodo = trpc.deleteTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
+
+  const changeTodo = trpc.changeTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
+
+  const [editId, setEditId] = useState<number | null>(null);
+  const [editTitle, setEditTitle] = useState("");
+  const [editDescription, setEditDescription] = useState("");
+
+  return (
+    <div className="flex items-start space-x-4 w-full" key={todo.id}>
+      <div className="self-center">
+        <Checkbox
+          id={`check-${todo.id}`}
+          checked={!!todo.completed}
+          onCheckedChange={() => {
+            setDone.mutate(todo.id);
+          }}
+        />
+      </div>
+
+      <div className="flex-1 self-center">
+        <div>
+          <label htmlFor={`check-${todo.id}`} className="block font-semibold">
+            {todo.title}
+          </label>
+          <p className="text-sm text-muted-foreground">{todo.description}</p>
+        </div>
+      </div>
+
+      <div className="flex flex-row gap-2 self-center">
+        <Button
+          variant="destructive"
+          size="sm"
+          onClick={() => deleteTodo.mutate(todo.id)}
+        >
+          <Trash2 />
+        </Button>
+
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => {
+                setEditId(todo.id);
+                setEditTitle(todo.title);
+                setEditDescription(todo.description);
+              }}
+            >
+              <Pencil />
+            </Button>
+          </AlertDialogTrigger>
+
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Editar tarefa</AlertDialogTitle>
+
+              <AlertDialogDescription>
+                <div className="space-y-2 flex flex-row justify-between">
+                  <div className="flex flex-col self-center w-full">
+                    <Label htmlFor="title" className="font-semibold mb-3 mt-5">
+                      Título
+                    </Label>
+                    <Input
+                      value={editTitle}
+                      onChange={(e) => setEditTitle(e.target.value)}
+                      placeholder="Título"
+                      className="w-full mb-5"
+                    />
+                    <Label htmlFor="title" className="font-semibold mb-3">
+                      Informações adicionais
+                    </Label>
+                    <Input
+                      value={editDescription}
+                      onChange={(e) => setEditDescription(e.target.value)}
+                      placeholder="Descrição"
+                      className="w-full mb-5"
+                    />
+                  </div>
+                </div>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+
+            <AlertDialogFooter>
+              <AlertDialogCancel onClick={() => setEditId(null)}>
+                Voltar
+              </AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  changeTodo.mutate({
+                    id: todo.id,
+                    title: editTitle,
+                    description: editDescription,
+                    creationDate: todo.creationDate,
+                    completed: todo.completed,
+                  });
+                  setEditId(null);
+                }}
+              >
+                {" "}
+                Confirmar edições
+                <Check />
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
+      {/* )} */}
+    </div>
+  );
+}
--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -1,15 +1,12 @@
 "use client";
-import { useState } from "react";
 
 import { trpc } from "../_trpc/client";
 import { serverClient } from "../_trpc/serverClient";
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Card } from "@/components/ui/card";
 
 import NewTodo from "./NewTodo";
+import Todo from "./Todo";
 
 export default function TodoList({
   initialTodos,
@@ -22,128 +19,20 @@ export default function TodoList({
     refetchOnReconnect: false,
   });
 
-  const setDone = trpc.setDone.useMutation({
-    onSettled: () => {
-      getTodos.refetch();
-    },
-  });
-
-  const deleteTodo = trpc.deleteTodo.useMutation({
-    onSettled: () => {
-      getTodos.refetch();
-    },
-  });
-
-  const changeTodo = trpc.changeTodo.useMutation({
-    onSettled: () => {
-      getTodos.refetch();
-    },
-  });
-
-
-  const [editId, setEditId] = useState<number | null>(null);
-  const [editTitle, setEditTitle] = useState("");
-  const [editDescription, setEditDescription] = useState("");
-
   return (
-    <main className="mx-auto p-20">
+    <main className="mx-auto p-16">
       <h1 className="text-3xl font-bold mb-6 text-center">
         Gerenciador de Tarefas
       </h1>
 
-      <div className="space-y-4 mb-10">
+      <Card 
+        // className="flex items-start justify-between p-4 shadow-sm"
+        className="p-6 shadow-md lg:w-1/2 mx-auto mb-10 lg:h-80 lg:max-h-80"
+      >
         {getTodos?.data?.map((todo) => (
-          <Card
-            key={todo.id}
-            className="flex items-start justify-between p-4 shadow-sm"
-          >
-            <div className="flex items-start space-x-4 w-full">
-              <Checkbox
-                id={`check-${todo.id}`}
-                checked={!!todo.completed}
-                onCheckedChange={() => {
-                  setDone.mutate(todo.id);
-                }}
-              />
-
-              <div className="flex-1">
-                {editId === todo.id ? (
-                  <div className="space-y-2">
-                    <Input
-                      value={editTitle}
-                      onChange={(e) => setEditTitle(e.target.value)}
-                      placeholder="Título"
-                    />
-                    <Input
-                      value={editDescription}
-                      onChange={(e) => setEditDescription(e.target.value)}
-                      placeholder="Descrição"
-                    />
-                    <div className="flex gap-2">
-                      <Button
-                        variant="default"
-                        onClick={() => {
-                          changeTodo.mutate({
-                            id: todo.id,
-                            title: editTitle,
-                            description: editDescription,
-                            creationDate: todo.creationDate,
-                            completed: todo.completed,
-                          });
-                          setEditId(null);
-                        }}
-                      >
-                        Confirmar
-                      </Button>
-                      <Button
-                        variant="secondary"
-                        onClick={() => setEditId(null)}
-                      >
-                        Cancelar
-                      </Button>
-                    </div>
-                  </div>
-                ) : (
-                  <div>
-                    <label
-                      htmlFor={`check-${todo.id}`}
-                      className="block font-semibold"
-                    >
-                      {todo.title}
-                    </label>
-                    <p className="text-sm text-muted-foreground">
-                      {todo.description}
-                    </p>
-                  </div>
-                )}
-              </div>
-
-              {editId !== todo.id && (
-                <div className="flex flex-col gap-2">
-                  <Button
-                    variant="destructive"
-                    size="sm"
-                    onClick={() => deleteTodo.mutate(todo.id)}
-                  >
-                    Deletar
-                  </Button>
-                  <Button
-                    variant="outline"
-                    size="sm"
-                    onClick={() => {
-                      setEditId(todo.id);
-                      setEditTitle(todo.title);
-                      setEditDescription(todo.description);
-                    }}
-                  >
-                    Editar
-                  </Button>
-                </div>
-              )}
-            </div>
-          </Card>
+          <Todo key={todo.id} todo={todo}/>
         ))}
-      </div>
+      </Card>
 
       <NewTodo />
     </main>
